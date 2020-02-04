9068fa573ab71067c132943bbbdfa13c31dfcfd5
charlotte hues <hues.charlotte@gmail.com>
2020-02-04 21:11:01 +0000

    added some code to get the result

--- assets/script.js
+++ assets/script.js
@@ -22,12 +22,15 @@ const player1 = {
     name: 'player 1',
     marker: 'X',
     isFirstMove: false,
+    positions: [],
 }
 
 const player2 = {
     name: 'player 2',
     marker: 'O',
     isFirstMove: true,
+    positions: [],
+
 }
 
 const game = (() => {
@@ -40,9 +43,22 @@ const game = (() => {
         if(square.dataset.inside === 'empty') {return true};        
     }
     
-    const checkResult = (player) => {
-        const playerMarkerPositions = document.querySelectorAll(`.${player.marker}`); 
-        playerMarkerPositions.forEach(position => console.log(position.id))  
+    const getResult = (player, square) => {
+        console.log(square.id);
+        player.positions.push(square.id);
+        console.log(player.positions);
+        const winArr = [
+            ["0","1","2"], ["3","4","5"], ["6","7","8"], ["0","4","8"],
+            ["0","3","6"], ["1","4","7"], ["2","5","8"], ["2","4","6"] 
+        ]
+        console.log(winArr[0]);
+        const isWinner = (playerArr) => {
+            return winArr.some(function(winVal) {
+                return playerArr.includes(winVal[0] && winVal[1] && winVal[2])
+            })
+        }
+
+        console.log(isWinner(player.positions));
         
     }
 
@@ -50,24 +66,16 @@ const game = (() => {
         square.setAttribute('data-inside', player.marker);
         square.classList.add(player.marker);
         square.innerHTML = player.marker;
-        checkResult(player);
-        render(player);
     }
 
-    function handleTurn() {
-        if(!squareIsEmpty(this)) return;
-        placeMarker(turn, this);
-        turn = turn === player1 ? player2 : player1;
-        return turn;
+    function handleTurn(e) {
+        if(!squareIsEmpty(e.target)) return;
+        placeMarker(turn, e.target);
+        let result = getResult(turn, this);
+        console.log(result);
+        // turn = turn === player1 ? player2 : player1;
+        // return turn;
     }
 
-    function render(player) {
-        console.log(squares);
-
-
-        // const playerMarkers = squares.filter(squares.getAttribute('data-inside') === player.marker);
-    }
-
-    console.log(render());
 
 })();
