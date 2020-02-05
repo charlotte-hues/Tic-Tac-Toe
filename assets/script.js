afd05a511ec47e106b52c1507554ce2d89c5981e
charlotte hues <hues.charlotte@gmail.com>
2020-02-05 21:04:04 +0000

    created logic to check for win or draw

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 const gameBoard = (() => {
+    const squares = [];
     const grid = document.getElementById('grid-container');
     const gameBoardArray = [
         '','','',
@@ -12,10 +13,10 @@ const gameBoard = (() => {
         div.classList.add('square');
         div.setAttribute('data-inside', 'empty');
         div.setAttribute('id', index);
+        squares.push(div);
         grid.append(div);
     })
-
-
+    return {squares}
 })();
 
 const player1 = {
@@ -30,36 +31,23 @@ const player2 = {
     marker: 'O',
     isFirstMove: true,
     positions: [],
-
 }
 
 const game = (() => {
-    const squares = document.querySelectorAll('.square');
-    squares.forEach(square => square.addEventListener('click', handleTurn));
-
+    let totalMoves = 0;
     let turn = player1.isFirstMove === true ? player1 : player2;
-
-    const squareIsEmpty = (square) => {
-        if(square.dataset.inside === 'empty') {return true};        
+    
+    const isWinner = (playerArr) => {
+        const winArr = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6]]
+        return winArr.some(winVal => winVal.every(winPos => playerArr.includes(winPos)))
     }
     
     const getResult = (player, square) => {
-        console.log(square.id);
-        player.positions.push(square.id);
-        console.log(player.positions);
-        const winArr = [
-            ["0","1","2"], ["3","4","5"], ["6","7","8"], ["0","4","8"],
-            ["0","3","6"], ["1","4","7"], ["2","5","8"], ["2","4","6"] 
-        ]
-        console.log(winArr[0]);
-        const isWinner = (playerArr) => {
-            return winArr.some(function(winVal) {
-                return playerArr.includes(winVal[0] && winVal[1] && winVal[2])
-            })
-        }
-
-        console.log(isWinner(player.positions));
-        
+        const position = gameBoard.squares.indexOf(square);
+        player.positions.push(position);        
+        if(isWinner(player.positions)) return `${player.name} is the winner`;
+        if(totalMoves === 9) return "It's a Draw!";
+        return 'no result';
     }
 
     const placeMarker = (player, square) => {
@@ -68,14 +56,31 @@ const game = (() => {
         square.innerHTML = player.marker;
     }
 
-    function handleTurn(e) {
-        if(!squareIsEmpty(e.target)) return;
-        placeMarker(turn, e.target);
-        let result = getResult(turn, this);
-        console.log(result);
-        // turn = turn === player1 ? player2 : player1;
-        // return turn;
+    const squareIsEmpty = (square) => square.dataset.inside === 'empty';
+
+
+    const endOfGame = (result) => {
+        gameBoard.squares.forEach(square => square.removeEventListener('click', handleTurn));
+        updateDom(result);
     }
+
+    const nextTurn = () => {
+        turn = turn === player1 ? player2 : player1;
+        updateDom(`${turn.name} turn`);
+    }
+
+    const updateDom = (message) => console.log(message);
+
+    const handleTurn = (e) => {
+        if(!squareIsEmpty(e.target)) return;
+        totalMoves++;
+        placeMarker(turn, e.target);
+        let result = getResult(turn, e.target);
+        (result == 'no result') ? nextTurn() :  endOfGame(result);
+        return;
+    }
+
+    gameBoard.squares.forEach(square => square.addEventListener('click', handleTurn));
 
 
 })();
